79cdf182a3bb31015aec369568aa076f865ba80c
Maurits van der Schee <maurits@vdschee.nl>
2015-11-06 22:25:26 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program
+{
+    public static void Main (string[] args)
+    {
+        Console.WriteLine("Hello, Linux");
+        Console.WriteLine("Love from CoreCLR.");
+    }
+}
