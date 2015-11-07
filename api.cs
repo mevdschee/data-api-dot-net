4056de11a834fa84d6fa3bbd86d8fabe007a5f2f
Maurits van der Schee <maurits@vdschee.nl>
2015-11-07 01:09:31 +0100

    Initial commit (2)

---
+++ api.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+
+namespace testaspx
+{
+
+	public class api : System.Web.IHttpHandler
+	{
+
+		public void ProcessRequest (HttpContext context)
+		{
+			context.Response.Write ("test");
+		}
+
+		public bool IsReusable {
+			get {
+				return false;
+			}
+		}
+	}
+}
